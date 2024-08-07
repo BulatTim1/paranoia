7a15c27ea6d6923c5d5dedaaf2317dffdc69987a
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2024-08-07 02:41:00 +0000

    chore: Update aiogram import in globals.py

--- bot/globals.py
+++ bot/globals.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import Config
 from models import User, Session
