81a372606ec6b8a4076678ae2869ec31fffa991b
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2024-08-07 01:30:27 +0000

    chore: Add logging to login.py

--- bot/states/login.py
+++ bot/states/login.py
@@ -9,17 +9,17 @@ from aiogram.utils.markdown import hbold
 from aiogram import types
 from globals import bot, dp, User
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+import logging
 
 
 class Guid(StatesGroup):
     guid = State()
 
-
 @dp.message_handler(state=Guid.guid)
 async def add_category(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     user = User.get_user_by_guid(message.text.strip())
-    print(message.text.strip())
+    logging.info(message.text.strip())
     if user:
         res = user.init_user(user_id)
         if res:
