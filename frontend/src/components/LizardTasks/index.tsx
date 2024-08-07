51c1198b3af078c293ce9f5dfe6619f909725acc
ultrarazrab227 <78072227+ultrarazrab227@users.noreply.github.com>
2024-08-07 08:19:39 +0300

    Adding lizard tasks

--- frontend/src/components/LizardTasks/index.tsx
+++ frontend/src/components/LizardTasks/index.tsx
@@ -2,27 +2,29 @@ import {List, Section} from "@telegram-apps/telegram-ui";
 
 import "./index.css"
 import {useEffect, useState} from "react";
-
+import axios from "axios";
 
 export const LizardTasks = () => {
-    const [tasks, setTasks] = useState([]);
-
-    useEffect(() => {
-        fetch('/api/tasks')
-            .then(response => response.json())
-            .then(data => setTasks(data))
-            .catch(error => console.error('Error fetching tasks:', error));
-    }, []);
 
     return (
         <div>
-            <List>
-                {tasks.map(task => (
-                    <Section key={task.id}>
-                        <div>{task.description}</div>
-                    </Section>
-                ))}
-            </List>
+            <h1 className="text-center">List of Books</h1>
+            <br />
+            <Row xs={1} md={2} className="g-4">
+                {books &&
+                    books.map((book, id) => (
+                        <Col key={id}>
+
+                            <Card key={id}>
+
+                                <Card.Body>
+                                    <Card.Title>{book.title}</Card.Title>
+                                    <Card.Text>{book.description}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+            </Row>
         </div>
     );
-}
+};
