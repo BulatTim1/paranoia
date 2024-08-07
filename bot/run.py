34e76ec49ad7e0eff6ebed3aab32673e925fd65f
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2024-08-07 01:41:12 +0000

    chore: Update run.py to use asyncio.run for starting the bot

--- bot/run.py
+++ bot/run.py
@@ -3,12 +3,11 @@ import sys
 from aiogram import executor as ex
 from commands import start
 from globals import dp
-
+import asyncio
 
 async def bot_callback(x):
     print("Started")
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    ex.start_polling(dp, on_startup=bot_callback, skip_updates=True)
+    asyncio.run(ex.start_polling(dp, on_startup=bot_callback, skip_updates=True))
