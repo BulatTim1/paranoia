8f190853b898b1fbd67c3e84281334234b7869bf
ultrarazrab227 <78072227+ultrarazrab227@users.noreply.github.com>
2024-08-07 03:23:57 +0300

    Init auth

--- web/my-react-app/src/components/MenuList/index.tsx
+++ web/my-react-app/src/components/MenuList/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {Button, Navigation, Cell, Badge, Avatar, Info, Section, List} from "@telegram-apps/telegram-ui";
+import {retrieveLaunchParams} from '@telegram-apps/sdk';
 
 import "./index.css"
 import {useNavigate} from "react-router-dom";
 import {instruction, game_menu} from "../../menu_routes.ts";
-
 
 
 export const MainMenu = () => {
@@ -15,15 +15,25 @@ export const MainMenu = () => {
         navigate(path);
     }
 
+    const {initDataRaw} = retrieveLaunchParams();
+    console.log(initDataRaw)
+    // fetch('https://example.com/api', {
+    //     method: 'POST',
+    //     headers: {
+    //         Authorization: `tma ${initDataRaw}`
+    //     },
+    // });
+
+
     return (
         <List>
             <header><i>Правила игры</i></header>
             <Section>
                 {instruction.map((el) => (
                     <Cell onClick={() => handleClick(el.path)}
-                        before={<Avatar size={48}/>}
-                        after={<Navigation className="arrow"/>}
-                        subtitle={el.description}
+                          before={<Avatar size={48}/>}
+                          after={<Navigation className="arrow"/>}
+                          subtitle={el.description}
                     >
                         {el.text}
                     </Cell>
@@ -32,10 +42,10 @@ export const MainMenu = () => {
             <header><i>Игра</i></header>
             <Section>
                 {game_menu.map((el) => (
-                    <Cell  onClick={() => handleClick(el.path)}
-                        before={<Avatar size={48}/>}
-                        after={<Navigation className="arrow"/>}
-                        subtitle={el.description}
+                    <Cell onClick={() => handleClick(el.path)}
+                          before={<Avatar size={48}/>}
+                          after={<Navigation className="arrow"/>}
+                          subtitle={el.description}
                     >
                         {el.text}
                     </Cell>
