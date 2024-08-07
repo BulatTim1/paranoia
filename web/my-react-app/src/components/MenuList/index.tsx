4f584b4c7b4f11851d8bbd8706df315da1ec156d
naxsi300 <naxsi300@gmail.com>
2024-08-07 03:19:25 +0300

    partialy added gif icons to react

--- web/my-react-app/src/components/MenuList/index.tsx
+++ web/my-react-app/src/components/MenuList/index.tsx
@@ -1,28 +1,21 @@
 import React from "react";
-import {Button, Navigation, Cell, Badge, Avatar, Info, Section, List} from "@telegram-apps/telegram-ui";
+import { Button, Navigation, Cell, Badge, Avatar, Info, Section, List, Image } from "@telegram-apps/telegram-ui";
 
 import "./index.css"
-import {useNavigate} from "react-router-dom";
-import {instruction, game_menu} from "../../menu_routes.ts";
-
+import { useNavigate } from "react-router-dom";
+import { instruction, game_menu } from "../../menu_routes.ts";
 
 
 export const MainMenu = () => {
-
-    const navigate = useNavigate()
-
-    const handleClick = (path: string) => {
-        navigate(path);
-    }
 
     return (
         <List>
             <header><i>Правила игры</i></header>
             <Section>
                 {instruction.map((el) => (
-                    <Cell onClick={() => handleClick(el.path)}
-                        before={<Avatar size={48}/>}
-                        after={<Navigation className="arrow"/>}
+                    <Cell
+                        before={<Image size={48} src={el.image}></Image>}
+                        after={<Navigation className="arrow" />}
                         subtitle={el.description}
                     >
                         {el.text}
@@ -32,9 +25,9 @@ export const MainMenu = () => {
             <header><i>Игра</i></header>
             <Section>
                 {game_menu.map((el) => (
-                    <Cell  onClick={() => handleClick(el.path)}
-                        before={<Avatar size={48}/>}
-                        after={<Navigation className="arrow"/>}
+                    <Cell
+                        before={<Avatar size={48} />}
+                        after={<Navigation className="arrow" />}
                         subtitle={el.description}
                     >
                         {el.text}
--- web/my-react-app/src/menu_routes.ts
+++ web/my-react-app/src/menu_routes.ts
@@ -1,43 +1,51 @@
+import { Avatar } from "@telegram-apps/telegram-ui"
+
 export const instruction = [
     {
         index: 0,
         name: "Game rules",
         text: "Правила игры",
         description: "",
-        path: "/rules"
+        path: "/rules",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/nerd.gif?raw=true"
     },
     {
         index: 1,
         name: "Player types",
         text: "Типы игроков",
         description: "",
-        path: "/PlayerTypes"
+        path: "/PlayerTypes",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/0-moon.gif?raw=true"
     }
 ]
 export const game_menu = [
     {
         index: 0,
         name: "Suspects",
-        text: "Ввод подозвеваемых",
+        text: "Ввод подозреваемых",
         description: "3 попытки в раунд",
-        path: "/sus-input"
+        path: "/sus-input",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/0-moon.gif"
     }, {
         index: 1,
         name: "Lizard tasks",
         text: "Задания для Ящеров",
         description: "список задач",
-        path: "/lizard-tasks"
+        path: "/lizard-tasks",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/0-moon.gif"
     }, {
         index: 2,
         name: "Game results",
         text: "Подведение итогов",
         description: "голосование",
-        path: "/result"
+        path: "/result",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/0-moon.gif"
     }, {
         index: 3,
         name: "Overall rating",
         text: "Общий рейтинг",
         description: "баллы",
-        path: "/rating"
+        path: "/rating",
+        image: "https://github.com/goforbg/telegram-emoji-gifs/blob/master/0-moon.gif"
     },
 ]
